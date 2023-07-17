b4859c4b683b21a878625c24e5782a2a2b31e9ed
Mr. Uzair <62235689+uzairdev07@users.noreply.github.com>
2023-07-17 07:18:15 +0500

    total_money_button.dart Pixel error fix

--- lib/widgtes/total_money_button.dart
+++ lib/widgtes/total_money_button.dart
@@ -18,50 +18,57 @@ class TotalMoneyButton extends StatefulWidget {
 class _TotalMoneyButtonState extends State<TotalMoneyButton> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8.0),
-      height: 50,
-      decoration: BoxDecoration(
-        color: widget.bgColor,
-        borderRadius: BorderRadius.circular(4.0),
-      ),
-      child: Row(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(2.0),
-            decoration: BoxDecoration(
-              color: widget.bgColor?.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(25.0),
-            ),
-            child: Icon(
-              widget.icon,
-              color: Colors.white,
-              size: 16.0,
-            ),
-          ),
-          const SizedBox(
-            width: 8.0,
-          ),
-          Column(
-            children: [
-              Text(
-                'Rs. 92,208',
-                style: TextStyle(
-                  color: widget.textColor,
-                  fontWeight: FontWeight.bold,
-                ),
+    return FractionallySizedBox(
+      widthFactor:
+          0.9, // Adjust this value to control the width of the Container
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: widget.bgColor,
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+        child: Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(2.0),
+              decoration: BoxDecoration(
+                color: widget.bgColor?.withOpacity(0.8),
+                borderRadius: BorderRadius.circular(25.0),
               ),
-              Text(
-                'Maine lene hain',
-                style: TextStyle(
-                  fontSize: 10.0,
-                  color: widget.textColor,
-                  fontWeight: FontWeight.bold,
-                ),
+              child: Icon(
+                widget.icon,
+                color: Colors.white,
+                size: 16.0,
               ),
-            ],
-          ),
-        ],
+            ),
+            const SizedBox(
+              width: 8.0,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Rs. 92,208',
+                    style: TextStyle(
+                      color: widget.textColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Maine lene hain',
+                    style: TextStyle(
+                      fontSize: 10.0,
+                      color: widget.textColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
